50ba21fd3b396e6e3961fbcc45f6b2c5069cd2f9
KonMarkowski <kon.markowski@gmail.com>
2023-05-25 21:58:06 +0200

    fix

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import SeoHead from "../components/SeoHead";
 export default function Home() {
   return (
     <>
-      <SeoHead title='Projekt Plus' />
+      <SeoHead title='Projekt Plusgi' />
       <Layout>
         <Hero />
         <Feature />
